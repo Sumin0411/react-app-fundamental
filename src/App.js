bac93063f811400e2b5aa677e2cd61c70ec10dbf
Sumin0411 <kimsumin411@gmail.com>
2024-03-15 20:54:31 +0900

    Add props

--- src/App.js
+++ src/App.js
@@ -11,19 +11,17 @@ function Header(props) {
     </header>
   );
 }
-function Nav() {
+function Nav(props) {
+    const lis = [
+    ]
+    for(let i =0; i<props.topics.length; i++){
+        let t = props.topics[i];
+        lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    }
   return (
     <nav>
       <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
+        {lis}
       </ol>
     </nav>
   );
@@ -35,10 +33,16 @@ function Article(props) {
   </article>
 }
 function App() {
+    const topics =[
+    {id : 1, title:'html', body:'html is ...'},
+    {id : 2, title:'css', body:'css is .....'},
+    {id : 3, title:'js', body:'js is ......'}
+]
+
   return (
     <div>
       <Header title="REACT"></Header>
-      <Nav></Nav>
+      <Nav topics={topics}></Nav>
       <Article title = "Welcome" body = "Hello WEB"></Article>
     </div>
   );
